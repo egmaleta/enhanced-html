aff728316062f7d07efbe9fe1c1341a59aa4a307
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-10-28 14:25:19 -0400

    shorten template attrs

--- packages/enhanced-html/src/attrs.ts
+++ packages/enhanced-html/src/attrs.ts
@@ -1,10 +1,10 @@
 export type EhAttr = `eh-${string}` | "eh";
 
 export const FOR_ATTR = "eh-for";
-export const FROM_TEMPLATE_ATTR = "eh-from-template";
+export const FROM_TEMPLATE_ATTR = "eh-from-templ";
 export const KEY_ATTR = "eh-key";
 
 // used by devs
 export const EH_ATTR = "eh";
-export const TEMPLATE_ATTR = "eh-template";
+export const TEMPLATE_ATTR = "eh-templ";
 export const PROPS_ATTR = "eh-props";
