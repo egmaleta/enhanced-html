47d5927dc3f58439517fbb625ba01386d99bdfd0
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-10-29 10:01:34 -0400

    remove eh attr requirement in scripts/styles inside template

--- packages/enhanced-html/src/template.ts
+++ packages/enhanced-html/src/template.ts
@@ -1,4 +1,4 @@
-import { EH_ATTR, TEMPLATE_ATTR } from "./attrs";
+import { TEMPLATE_ATTR } from "./attrs";
 import { handle as handleScript } from "./script";
 import { handle as handleStyle } from "./style";
 import { isHTMLElement, isTaggedHTMLElement, tokenizeAttr } from "./utils";
@@ -14,7 +14,7 @@ export function handle(element: HTMLElement) {
     if (!template) continue;
 
     for (const child of template.content.childNodes) {
-      if (isHTMLElement(child) && child.hasAttribute(EH_ATTR)) {
+      if (isHTMLElement(child)) {
         const isScript = isTaggedHTMLElement(child, "SCRIPT");
         if (isScript || isTaggedHTMLElement(child, "STYLE")) {
           isScript
