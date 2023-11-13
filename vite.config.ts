a286bb6e99d4ff285465d15a1d4d2d82654ba516
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-11-12 19:12:07 -0500

    rename global object

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      name: "eh",
+      name: "Eh",
       formats: ["umd"],
       fileName: () => "eh.js",
     },
